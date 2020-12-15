aaa8daefd1520f9c8c63b564752ff90eca934896
TritriLe10E <tristan2003@orange.fr>
2020-12-15 14:43:28 +0100

    sphere et respawn

---
+++ Assets/respawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 spawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= -3)
+        {
+            transform.position = spawn;
+        }
+
+        Debug.Log(transform.position);
+    }
+}
