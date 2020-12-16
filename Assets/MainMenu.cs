c65c334a7f99a40b8a9af207cef533e1a9112fdf
TritriLe10E <tristan2003@orange.fr>
2020-12-16 14:43:55 +0100

    boutton quit

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,4 +9,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("quit");
+    }
 }
