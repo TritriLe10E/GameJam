c7c76a429d0837ad17ac30551d87b10dbd2c00a7
TritriLe10E <tristan2003@orange.fr>
2020-12-17 16:21:28 +0100

    decolage

--- Assets/HQ_ResidentialHouse/Scripts/BallonLaHaut.cs
+++ Assets/HQ_ResidentialHouse/Scripts/BallonLaHaut.cs
@@ -7,15 +7,18 @@ public class BallonLaHaut : MonoBehaviour
     [SerializeField]
     GameObject ball;
     static float currentX = -6f;
-
-
+    public static int nombreBallon;
+    
     private void OnTriggerEnter(Collider other)
     {
         Vector3 position = new Vector3(currentX, 18f, 0.16f);
         Instantiate(ball, position, transform.rotation);
         Destroy(other.gameObject);
         currentX += 3f;
+        nombreBallon++;
     }
+
+
 
 }
 
